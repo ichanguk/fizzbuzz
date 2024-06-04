8ba353232882ca2d15187511540451de92532a18
changukLee <102301656+ichanguk@users.noreply.github.com>
2024-06-04 20:25:20 +0900

    fix: fix cant' print fizzbuuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,9 +1,9 @@
 for i in range(1, 20 + 1):
-    if i % 3 == 0:
-        print('fizz')
+    if i % 5 == 0 and i % 3 == 0:    
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
     elif i % 5 == 0:
         print('Buzz')
-    elif i % 5 == 0 and i % 3 == 0:    
-        print('FizzBuzz')
     else:
         print(i)
