79457b042a0334117894b3d17e811c860408b313
changukLee <ckddnr5527@gmail.com>
2024-06-04 20:21:26 +0900

    fix: fix can't print fizzbuzz bug

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,9 @@
 for i in range(1, 20 + 1):
-    if i % 3 == 0:
-        print('fizz')
-    if i % 5 == 0:
-        print('buzz')
+    if i % 5 == 0 and i % 3 == 0:    
+        print('FizzBuzz')
+    elif i % 3 == 0:
+        print('Fizz')
+    elif i % 5 == 0:
+        print('Buzz')
+    else:
+        print(i)
